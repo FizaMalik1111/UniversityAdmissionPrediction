262fe514cbae1245d6b59ca42b68d2c41044c258
Fiza Malik <fiza95028@gmail.com>
2024-08-12 13:27:25 +0530

    Update app_final_run.py

--- app_final_run.py
+++ app_final_run.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 import pickle 
 
-with open("case_study_university.pkl", "wb") as f:
+#with open("case_study_university.pkl", "wb") as f:
     pickle.dump(model, f)
-#clf = pickle.load(open("case_study_university.pkl","rb"))
+clf = pickle.load(open("case_study_university.pkl","rb"))
 #import pickle
 #pickle.dump(rf,open("case_study_university.pkl","wb"))
 
