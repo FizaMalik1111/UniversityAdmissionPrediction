ea9ad2d011ae33ca6783be71c2dbabba64645063
Fiza Malik <fiza95028@gmail.com>
2024-08-12 13:46:09 +0530

    Update app_final_run.py

--- app_final_run.py
+++ app_final_run.py
@@ -3,14 +3,11 @@ import pandas as pd
 import numpy as np
 import pickle 
 
-#with open("case_study_university.pkl", "wb") as f:
-    #pickle.dump(model, f)
-clf = pickle.load(open("case_study_university.pkl","rb"))
-#import pickle
-#pickle.dump(rf,open("case_study_university.pkl","wb"))
+
+clf = pickle.load(open("university.pkl","rb"))
 
 def predict(data):
-    clf = pickle.load(open("case_study_university.pkl","rb"))
+    clf = pickle.load(open("university.pkl","rb"))
     return clf.predict(data)
 
 st.title("Case Study On University Admission Prediction")
